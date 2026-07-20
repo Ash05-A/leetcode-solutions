0faf07171a119d9083bef180be4cdfd3fcd61008
Yashda Aher <yashdaaher@gmail.com>
2026-07-21 00:44:04 +0530

    Time: 0 ms (100%), Space: 7.8 MB (50.17%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int s;
+       if( s=n>0&&(n&(n-1))==0)
+       { return true;
+      
+    }
+    
+      return false;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
