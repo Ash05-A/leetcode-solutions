ad2e23420e76668103deddda531b210fe86ef514
Yashda Aher <yashdaaher@gmail.com>
2026-07-22 23:58:17 +0530

    Time: 4 ms (10.34%), Space: 11.7 MB (13.86%) - LeetHub - Time: O(n * n!), Space: O(n * n!)

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    void solve(vector<int> nums,int index,vector<vector<int>> &ans)
+    {
+        if(index>=nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        for(int j=index;j<nums.size();j++)
+        {
+            swap(nums[index],nums[j]);
+            solve(nums,index+1,ans);
+             swap(nums[index],nums[j]);
+
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+      vector<vector<int>> ans;
+      int index=0;
+      solve(nums,index,ans);
+      return ans;  
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
